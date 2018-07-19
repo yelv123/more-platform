531e2df150a7ca45acb7152c8d508c05ef3cc80e
yelv123 <swustncgis@gmail.com>
2018-07-19 13:47:27 +0800

    init

---
+++ src/MorePlatformClient.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/19 0019
+ * Time: 13:30
+ */
+namespace MorePlatform;
+use  GuzzleHttp\Client;
+use  App\Model\PlatformAccount;
+class MorePlatformClient
+{
+        public function __construct()
+        {
+
+
+        }
+
+        /**
+        * @param Platform $platformAccount 登录接口
+        */
+        public function login(Platform $platformAccount)
+        {
+
+        }
+
+        /**
+        * @param Platform $platformAccount 发送短信
+        */
+        public function smsCode(Platform $platformAccount)
+        {
+
+        }
+}
